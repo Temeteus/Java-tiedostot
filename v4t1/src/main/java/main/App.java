397de57ce06aedc8123e9d4e299686dceb7b46ba
Temeteus <teemumikael@hotmail.fi>
2026-07-28 13:54:36 +0300

    v4t1 fix 1.2

--- v4t1/src/main/java/main/App.java
+++ v4t1/src/main/java/main/App.java
@@ -41,7 +41,6 @@ public class App {
                     break;
 
                 case 2:
-                    System.out.println("Luolan hirviöt:");
                     cave.listMonsters();
                     break;
 
